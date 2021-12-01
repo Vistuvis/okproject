ff2cfa0cb65dfd4a3c89099f4f51728d5802c980
Ocean-Server <sir.wyatt@gmail.com>
2021-12-01 04:16:40 +0000

    commit on 'main'

--- login.php
+++ login.php
@@ -11,13 +11,13 @@ $mysqli = mysqli_connect($server, $user, $pass, $database) or die("Connection fa
 
 $username = filter_input(INPUT_POST, "username");
 $password = filter_input(INPUT_POST, "password");
-
+echo $username;
 $sql = "SELECT * FROM Members WHERE userName = '$username' AND password = SHA1('$password')";
 $result = mysqli_query($mysqli, $sql);
 
 if (mysqli_num_rows($result) == 0) {	
 	if (!filter_input(INPUT_POST, "username") || !filter_input(INPUT_POST, "password")) {
-		$_POST['loginfailed'] = "Username or password is incorrect";
+		$_POST["loginfailed"] = "Username or password is incorrect";
 		header("Location: login.html");
 		exit;
 	}
